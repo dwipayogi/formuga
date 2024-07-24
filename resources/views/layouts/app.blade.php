73523a48bdec9e508773f6544eea0e4c0ce1fbd5
Dwipa Yogiswara <dwipayogi50@gmail.com>
2024-07-24 09:49:23 +0700

    homepage design changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased" x-data="{ darkMode: false }" x-init="if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {localStorage.setItem('darkMode', JSON.stringify(true));} darkMode = JSON.parse(localStorage.getItem('darkMode')); $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" x-cloak>
         <div  x-bind:class="{'dark' : darkMode === true}" class="bg-gray-100" >
